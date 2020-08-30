74c570eff7c5e6481749c288c309cf5c50e7f7fc
Aditi Saxena <adyy.111@rediffmail.com>
2020-08-30 15:17:37 +0530

    Adding the Code changes for DanskeIT coding round

---
+++ ParsePyramid/ParsePyramid/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ParsePyramid
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string input;
+        //    int _maxRows = 0;
+        //    int _maxColumns = 0;
+        //    string start = string.Empty;
+        //    Console.WriteLine("Input your text (type EXIT to terminate): ");
+
+        //    do
+        //    {
+        //        input = Console.ReadLine();
+        //        if (!string.IsNullOrEmpty(input) && !string.IsNullOrWhiteSpace(input))
+        //        {
+        //            if (input.ToUpper() != "EXIT")
+        //            {
+        //                start = start + input + "\n";
+        //            }
+        //        }
+        //        else
+        //        {
+        //            continue;
+        //        }
+        //    } while (input.ToUpper() != "EXIT");
+
+        //    Pyramid pyr = new Pyramid();
+        //    Console.WriteLine(pyr.ReadContent(start));
+        //    Console.ReadKey();
+        //}
+        static void Main(string[] args)
+        {
+            try
+            {
+                int choices;
+                do
+                {
+                    Console.WriteLine("Enter your choice number");
+                    Console.WriteLine("1 Enter data");
+                    Console.WriteLine("2 Clear Console");
+                    Console.WriteLine("3 or any other key to exit");
+                    var m = Console.ReadLine().ToString();
+                    choices = (string.IsNullOrEmpty(m) || string.IsNullOrWhiteSpace(m)) ? 0 : Convert.ToInt32(m);
+
+                    switch (choices)
+                    {
+                        case 1:
+                            ReadFromConsole();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Environment.Exit(0);
+                            break;
+                        case 0:
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        default:
+                            Environment.Exit(0);
+                            break;
+                    }
+
+                } while (choices == 1 || choices == 2 || choices == 3 || choices == 0);
+            }
+            catch (Exception)
+            {
+                Console.ReadKey();
+                Environment.Exit(0);
+            }            
+        }
+
+        public static void ReadFromConsole()
+        {
+            string start = string.Empty;
+            string input;
+            Console.WriteLine("Input your text (type END to terminate): ");
+            do
+            {
+                input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input) && !string.IsNullOrWhiteSpace(input))
+                {
+                    if (input.ToUpper() != "END")
+                    {
+                        start = start + input + "\n";
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            } while (input.ToUpper() != "END");
+            Pyramid pyr = new Pyramid();
+            if (!pyr.IsValidBinaryTree(start))
+            {
+                // In case of Invalid binary tree - do processing required 
+            }
+            else
+            {
+                // Processing only valid Binary tree
+                string result = pyr.ProcessContent(start);
+                Console.WriteLine(result);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ ParsePyramid/ParsePyramid/Pyramid.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ParsePyramid
+{
+    public  class Pyramid: PathAndValues
+    {
+        public string inputString;
+        public int _maxRows;
+        public int _maxColumns;
+        public int _maxSum;
+        public static List<PathAndValues> AllPaths = new List<PathAndValues>();
+        public static PathAndValues temp;
+        public string ProcessContent(string input)
+        {           
+            var temp = input.Split('\n').Select(y => y).ToList();
+            _maxRows = temp.ToList().Count();
+            _maxColumns = temp[_maxRows - 2].Split(' ').Select(z => z).ToList().Count();            
+            int[,] list = new int[_maxRows-1, _maxColumns];
+            var charArray = input.Split('\n');
+            bool InvalidEntryFlag = false;            
+            try
+            {
+                for (int i = 0; i < charArray.Length; i++)
+                {                   
+                    var numArr = charArray[i].Trim().Split(' ');
+                    var numbersList = numArr.ToList().Where(y => !string.Equals(y, ""));
+                    numArr = numbersList.ToArray();
+                    if (!InvalidEntryFlag)
+                    {
+                        for (int j = 0; j < numArr.Length; j++)
+                        {
+                            if (!string.IsNullOrWhiteSpace(numArr[j]) && !string.IsNullOrEmpty(numArr[j]))
+                            {
+                                int number = Convert.ToInt32(numArr[j]);
+                                list[i, j] = number;
+                                if (j > i)
+                                {
+                                    Console.WriteLine("Invalid Number of elements in row {0}", i);
+                                    InvalidEntryFlag = true;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (!InvalidEntryFlag)
+                {
+                    AllPaths = new List<PathAndValues>();
+                    var result = TraverseThroughOddEvenNodes(charArray, list, AllPaths, 0, 0, "", 0);
+
+                    if (result.Count > 0)
+                    {
+                        int maxpath = result.Max(y => y.MaxVal);
+                        result = result.Where(y => y.MaxVal == maxpath).ToList();
+                        Console.WriteLine("\nPath(s) with maximum sum of the numbers:");
+                        foreach (var t in result)
+                        {
+                            Console.WriteLine(t.PathString.TrimStart('-').TrimStart('>') + "   Value: " + t.MaxVal);
+                        }
+
+                        return "";
+                    }
+                    else
+                    {
+                        return "No odd Even Paths possible from root to bottom of the Pyramid";                        
+                    }
+                }
+                else
+                {
+                    return "Invalid Pyramid\\Binary tree";
+                }
+            }
+            catch (Exception)
+            {                
+                return "Invalid Pyramid\\Binary tree";
+            }
+        }
+
+
+        // Calling the TraverseThroughOddEvenNodes in recursion to traverse through odd even nodes in left and right nodes
+        public static List<PathAndValues> TraverseThroughOddEvenNodes(string[] arrayOfRowsByNewlines, int[,] tableHolder, List<PathAndValues> pathAndValues, int rows = 0, int columns = 0, string path = "", int maxSum = 0)
+        {       
+          
+            int _maxLevels = arrayOfRowsByNewlines.Length - 2;
+            int i = rows;
+            int j = columns;
+            maxSum = maxSum + tableHolder[i, j];
+            path = path + "->" + tableHolder[i, j].ToString();
+            if (rows < _maxLevels)
+            {
+                if ((tableHolder[i + 1, j] + tableHolder[i, j]) % 2 != 0) // parse left node incase of odd-even or vice versa pair of parent-node and chile-node
+                {
+                    TraverseThroughOddEvenNodes(arrayOfRowsByNewlines, tableHolder, pathAndValues, i + 1, j, path, maxSum);
+                }
+                if ((tableHolder[i + 1, j + 1] + tableHolder[i, j]) % 2 != 0) // parse right node incase of odd-even or vice versa pair of parent-node and chile-node
+                {
+                    TraverseThroughOddEvenNodes(arrayOfRowsByNewlines, tableHolder, pathAndValues, i + 1, j + 1, path, maxSum);
+                }
+            }
+            else
+            {
+                temp = new PathAndValues
+                {
+                    MaxVal = maxSum,
+                    PathString = path
+                };
+                AllPaths.Add(temp); // add path and values to the list
+            }
+            return AllPaths;
+        }
+
+        //Validate the Input
+        public bool IsValidBinaryTree(string str)
+        {
+            Regex regex = new Regex("^-[0-9]+$");
+            var check = str.Split('\n').ToList();
+            int TotalRows = check.Count - 1;
+            string errorMessage = "Invalid Pyramid\\BinaryTree \nSmallest Binary tree should have 2 level with 1 root and 2 element at last level";
+            if (TotalRows == 1) // Pyramid\binary tree with only root element
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+            else if (TotalRows == 2 && check[1].Split(' ').ToList().Where(z => !string.Equals(z, ' ')).Count() < 2)// Pyramid\binary tree with only root element and one child
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+            else if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str) || str.ToLower().Equals("end"))// invalid spaces
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+            else
+            {
+                return true;
+            }           
+        }
+    }
+
+    public class PathAndValues
+    {
+        public string PathString;
+        public int MaxVal;
+    }
+}
